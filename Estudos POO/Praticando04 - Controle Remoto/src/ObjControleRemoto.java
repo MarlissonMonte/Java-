3f333b2ba73039b47b6f2640fd4572681e3a4ef2
MarlissonMonte <92329431+MarlissonMonte@users.noreply.github.com>
2022-08-01 21:22:37 -0300

    Encapsulamento

---
+++ Estudos POO/Praticando04 - Controle Remoto/src/ObjControleRemoto.java
@@ -0,0 +1,9 @@
+public class ObjControleRemoto {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.maisVolume();
+        c.pause();
+        c.abrirMenu();
+    }
+}
